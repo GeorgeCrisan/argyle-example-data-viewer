45fff488489c5aa8250e7236ea836df8278d538f
Ruta <ruta@argyle.io>
2020-01-24 10:47:15 +0200

    cleanup

---
+++ src/theme/globalStyles.js
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from 'styled-components'
+import globalStyleRules from './globalStyleRules'
+
+const GlobalStyle = createGlobalStyle`${globalStyleRules}`
+
+export default GlobalStyle
---
+++ src/theme/index.js
@@ -0,0 +1,33 @@
+const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  successGreen: '#32cd38',
+  borderBottom: 'rgba(0, 0, 0, 0.1)',
+  privacyLink: 'rgba(0, 0, 0, 0.2)',
+  placeHolder: 'rgba(0, 0, 0, 0.3)',
+  linkGrey: 'rgba(0, 0, 0, 0.4)',
+  darkGrey: 'rgba(0, 0, 0, 0.8)',
+  danger: '#e72842',
+  warning: '#ec9c18',
+  neutralGrey: '#d9d9d9',
+  backgroundGrey: '#f4f4f4',
+  cornFlowerBlue: '#4c7cda',
+  cornFlowerBlueLight: 'rgba(76, 124, 218, 0.5)',
+  transparentGrey: 'rgba(0, 0, 0, 0.05)',
+  wheat: '#fed88a',
+  transparentBlack: 'rgba(0, 0, 0, 0.7)',
+  modalBackdrop: 'rgba(216, 216, 216, 0.8)',
+  transparentWhite: 'rgba(255, 255, 255, 0.4)'
+}
+
+const mediaQueries = {
+  maxTabletWidth: '@media only screen and (max-width: 1023px)',
+  maxMobileWidth: '@media only screen and (max-width: 767px)'
+}
+
+const theme = {
+  colors,
+  mediaQueries
+}
+
+export default theme
