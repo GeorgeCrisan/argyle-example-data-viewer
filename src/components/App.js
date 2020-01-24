1c530dedc2c0634e9a0ac28c64b171a8aed5c22c
Valdemaras Repšys <valdemaras@gmail.com>
2020-01-24 11:07:30 +0200

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const App = () => <div>app</div>;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        Main
+      </Route>
+      <Route exact path="/start">
+        Enter your details
+      </Route>
+    </Switch>
+  </Router>
+)
 
-export default App;
+export default App
