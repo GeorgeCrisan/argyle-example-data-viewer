cdff143cccf47316046212f2bd3b538268cf3c2a
Ruta <ruta@argyle.io>
2020-02-18 17:38:50 +0200

    remove promise

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,4 @@
-export const signIn = ({ email, password }) =>
-  new Promise(resolve => {
-    localStorage.setItem('clientID', email)
-    localStorage.setItem('clientSecret', password)
-    resolve()
-  })
+export const signIn = ({ email, password }) => {
+  localStorage.setItem('clientID', email)
+  localStorage.setItem('clientSecret', password)
+}
